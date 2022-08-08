729b564bd7de7966fa5785d8705a9a1d8f0a503f
Tim Stannard <stannard.tim@gmail.com>
2022-08-08 13:31:55 +1200

    added dinos

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ app.use(cors())
 
 const data = [
     {
-        name: "carrot",
+        name: "cool carrot",
         colour: "orange"
     },
     {
@@ -15,6 +15,22 @@ const data = [
     },
     {
         name: "yam",
+        colour: "red"
+    }
+]
+
+
+const dinos = [
+    {
+        name: "triceretops",
+        colour: "orange"
+    },
+    {
+        name: "swagceratops",
+        colour: "green"
+    },
+    {
+        name: "billosaurus",
         colour: "red"
     }
 ]
@@ -29,9 +45,15 @@ app.get('/', (req, res) => {
     `)
 })
 
-// set up the route for the about page
+// set up the route for the vegetable page
+// req is short for request
+// res is short for response
 app.get('/vegetables', (req, res) => {
     res.send(data)
+})
+
+app.get('/dinos', (req, res) => {
+    res.send(dinos)
 })
 
 // Get the route for /contact and show an h1 saying wellcome to contact page
@@ -44,3 +66,13 @@ app.listen(port, () => {
     console.log(`Our app is running on port ${port}`)
 })
 
+// Activity:
+// Backend:
+
+// Create an array filled with objects.
+// Add in 3 dinosaurs, have an image url and a name.
+
+// Create a route which is "/dinosaurs". Send
+// the dinosaur data to that route as a response.
+
+// In the front endpointURL, show the dinos using an AJAX request.
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,11 +1,38 @@
 // declare our settings
 const endpointURL = "http://localhost:3000/vegetables";
+const result = document.getElementById('result');
+console.log("hello");
+
+let showData = (data) => {
+
+    data.forEach(item => {
+        result.innerHTML += `
+        ${item.name} ${item.colour} <br>
+        `
+    });
+}
 
 $.ajax({
     type: 'GET',
     url: endpointURL,
     success: function (data) {
         console.log(data);
+        showData(data);
+
+    },
+    error: function (error) {
+        console.log(error);
+    }
+})
+
+
+$.ajax({
+    type: 'GET',
+    url: "http://localhost:3000/dinos",
+    success: function (data) {
+        console.log(data);
+        showData(data);
+
     },
     error: function (error) {
         console.log(error);
